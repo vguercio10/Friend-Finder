478d6d938944f274649ae01a2824b4457cd0a3d1
Jessica Jones <47366649+vguercio10@users.noreply.github.com>
2019-04-03 15:08:02 -0400

    added connection in server.js

---
+++ FriendFinder/server.js
@@ -0,0 +1,27 @@
+// Require dependencies
+var express = require("express");
+var path = require("path");
+
+// Creating the express app instance 
+var app = express();
+
+// Setting the port 
+var PORT = process.env.PORT || 8080;
+
+// Set up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static('public'));
+
+// Requiring api and html routes
+require("app/routing/apiRoutes")(app);
+require("app/routing/htmlRoutes")(app);
+
+// Start our server so that it can begin listening to client requests.
+app.listen(PORT, function() {
+    // Log (server-side) when our server has started
+    console.log("Server listening on: http://localhost:" + PORT);
+  });
+
+
